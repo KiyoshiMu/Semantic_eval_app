b43a84af067ce230c1c1bdd4c61f341440c8c274
YewtsingMu <mooyewtsing@gmail.com>
2020-11-02 09:40:55 -0500

    stable online eval

--- lib/evaluation/evaluation_provider.dart
+++ lib/evaluation/evaluation_provider.dart
@@ -7,35 +7,55 @@ import 'package:http/http.dart' as http;
 
 class EvaluationProvider {
   static const resultPath = 'assets/eval.json';
-  static const url = "https://gosheet-bqjlnzid4q-uc.a.run.app/done";
+  static const doneUrl = "https://gosheet-bqjlnzid4q-uc.a.run.app/done";
+  static const evalUrl = "https://gosheet-bqjlnzid4q-uc.a.run.app/add";
 
   List<EvaluationModel> results;
   EvaluationProvider();
+  Set<int> doneIndices;
 
   Future<EvaluationModel> fetchEvaluation(int index) async {
     if (results == null) {
-      final resultString = await getFileData(resultPath);
-      results = parseResults(resultString);
+      final resultString = await _getFileData(resultPath);
+      results = _parseResults(resultString);
     }
     final done = await getDone();
     final isDone = done.contains(index);
     return results[index].markDone(isDone);
   }
 
-  Future<String> getFileData(String path) async {
+  Future<String> _getFileData(String path) async {
     return await rootBundle.loadString(path);
   }
 
-  List<EvaluationModel> parseResults(String resultString) {
+  List<EvaluationModel> _parseResults(String resultString) {
     final data = json.decode(resultString) as List;
     final output = data.map((json) => EvaluationModel.fromJson(json)).toList();
     return output;
   }
 
   Future<Set<int>> getDone() async {
-    final response = await http.get(url);
+    if (this.doneIndices != null) {
+      return this.doneIndices;
+    }
+    final response = await http.get(doneUrl);
     final data = json.decode(response.body) as List;
     final output = data.skip(1).map((e) => int.parse(e[0])).toSet();
+    this.doneIndices = output;
     return output;
+  }
+
+  Future<void> writeEval(EvaluationModel evaluationModel) async {
+    final content = evaluationModel.toJudge();
+    _apiRequest(evalUrl, content);
+    this.doneIndices.add(evaluationModel.id);
+  }
+
+  Future<void> _apiRequest(String url, Map jsonMap) async {
+    await http.post(url,
+        headers: const <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(jsonMap));
   }
 }
--- lib/evaluation/evaluation_repository.dart
+++ lib/evaluation/evaluation_repository.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:eval_app/evaluation/index.dart';
-import 'package:http/http.dart' as http;
 
 class EvaluationRepository {
   final EvaluationProvider _evaluationProvider = EvaluationProvider();
-  static const evalUrl = "https://gosheet-bqjlnzid4q-uc.a.run.app/add";
 
   EvaluationRepository();
 
@@ -19,15 +16,6 @@ class EvaluationRepository {
   }
 
   Future<void> writeEval(EvaluationModel evaluationModel) async {
-    final content = evaluationModel.toJudge();
-    apiRequest(evalUrl, content);
+    return _evaluationProvider.writeEval(evaluationModel);
   }
-}
-
-Future<void> apiRequest(String url, Map jsonMap) async {
-  await http.post(url,
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(jsonMap));
 }
